0b1dd642d56b9434bad19875a29e0e76d1b2cdf7
basilkiwanuka <basilkiwanuka@icloud.com>
2024-01-22 07:37:47 -0500

    Added code to catalog services

--- src/test/java/com/mbsystems/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/mbsystems/catalogservice/CatalogServiceApplicationTests.java
@@ -4,13 +4,7 @@ import com.mbsystems.catalogservice.entity.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestClient;
-
-import java.util.List;
-import java.util.Objects;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,103 +13,101 @@ import static org.assertj.core.api.Assertions.assertThat;
 )
 class CatalogServiceApplicationTests {
 
-    @Configuration
-    static class ContextConfiguration {
-        @Bean
-        public RestClient restClient() {
-            return RestClient.create("https://jsonplaceholder.typicode.com");
-        }
-    }
-
     @Autowired
-    private RestClient restClient;
+    private WebTestClient webTestClient;
 
     @Test
     void whenGetRequestWithIdThenBookReturned() {
         var bookIsbn = "1231231230";
         var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
-        Book expectedBook = restClient
+
+        Book expectedBook = webTestClient
                 .post()
                 .uri("/books")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(bookToCreate)
-                .retrieve()
-                .body(Book.class);
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+                .returnResult().getResponseBody();
 
-        Book expectedBook2 = restClient
+        webTestClient
                 .get()
                 .uri("/books/" + bookIsbn)
-                .retrieve()
-                .body(Book.class);
-
-        assertThat(expectedBook2).isNotNull();
-        assertThat(expectedBook2.isbn()).isEqualTo(Objects.requireNonNull(expectedBook).isbn());
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
     }
     @Test
     void whenPostRequestThenBookCreated() {
         var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
 
-        Book expectedBook2 = restClient
+        webTestClient
                 .post()
                 .uri("/books")
-                .body(expectedBook)
-                .retrieve()
-                .body(Book.class);
-
-        assertThat(expectedBook2).isNotNull();
-        assertThat(expectedBook2.isbn()).isEqualTo(expectedBook.isbn());
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
     }
 
     @Test
     void whenPutRequestThenBookUpdated() {
         var bookIsbn = "1231231232";
         var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
-        Book createdBook = restClient
+
+        Book createdBook = webTestClient
                 .post()
                 .uri("/books")
-                .body(bookToCreate)
-                .retrieve()
-                .body(Book.class);
-
-        assertThat(createdBook).isNotNull();
-
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(book -> assertThat(book).isNotNull())
+                .returnResult().getResponseBody();
         var bookToUpdate = new Book(createdBook.isbn(), createdBook.title(), createdBook.author(), 7.95);
 
-        Book bookToUpdate2 = restClient
+        webTestClient
                 .put()
                 .uri("/books/" + bookIsbn)
-                .body(bookToUpdate)
-                .retrieve()
-                .body(Book.class);
-
-        assertThat(bookToUpdate2).isNotNull();
-        assertThat(bookToUpdate2.price()).isEqualTo(bookToUpdate.price());
+                .bodyValue(bookToUpdate)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.price()).isEqualTo(bookToUpdate.price());
+                });
     }
 
     @Test
     void whenDeleteRequestThenBookDeleted() {
         var bookIsbn = "1231231233";
         var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
-        Book bookToCreate2 = restClient
+
+        webTestClient
                 .post()
                 .uri("/books")
-                .body(bookToCreate)
-                .retrieve()
-                .body(Book.class);
+                .bodyValue(bookToCreate)
+                .exchange()
+                .expectStatus().isCreated();
 
-        restClient
+        webTestClient
                 .delete()
                 .uri("/books/" + bookIsbn)
-                .retrieve();
+                .exchange()
+                .expectStatus().isNoContent();
 
-//TODO need to write the error casew
-//        Book bookToCreate3 = restClient
+//        webTestClient
 //                .get()
 //                .uri("/books/" + bookIsbn)
-//                .retrieve()
-//                .body(List.class);
-//
-//        .value(errorMessage ->
+//                .exchange()
+//                .expectStatus().isNotFound()
+//                .expectBody(String.class)
+//                    .value(errorMessage ->
 //                        assertThat(errorMessage).isEqualTo("The book with ISBN " + bookIsbn + " was not found.")
 //                );
     }

6b7b61ed24cdf6fe15179e87a25f1bf64ea7c21d
basilkiwanuka <basilkiwanuka@icloud.com>
2024-01-23 05:24:50 -0500

    Added connection to Config Server

--- src/main/java/com/mbsystems/catalogservice/controller/HomeController.java
+++ src/main/java/com/mbsystems/catalogservice/controller/HomeController.java
@@ -1,15 +1,22 @@
 package com.mbsystems.catalogservice.controller;
 
+import com.mbsystems.catalogservice.config.PolarProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v1")
 public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
 
     @GetMapping(path = "/")
     public String getGreeting() {
-        return "Welcome to the book catalog";
+        return this.polarProperties.getGreeting();
     }
 }

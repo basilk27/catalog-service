e327bfb3437b9f690b0cf1b11f6dbb28a2c1622b
basilkiwanuka <basilkiwanuka@icloud.com>
2024-01-23 09:39:48 -0500

    Added connection to Config Server

--- src/main/java/com/mbsystems/catalogservice/config/PolarProperties.java
+++ src/main/java/com/mbsystems/catalogservice/config/PolarProperties.java
@@ -1,6 +1,5 @@
 package com.mbsystems.catalogservice.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "polar")
---
+++ src/test/java/com/mbsystems/catalogservice/repository/BookRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.mbsystems.catalogservice.repository;
+
+import com.mbsystems.catalogservice.config.DataConfig;
+import com.mbsystems.catalogservice.entity.Book;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("integration")
+class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+//    @BeforeEach
+//    void setUp() {
+//    }
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+        //given
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 12.90);
+
+        jdbcAggregateTemplate.insert(book);
+
+        //when
+        Optional<Book> results = this.bookRepository.findByIsbn(bookIsbn);
+
+        //then
+        assertThat(results).isPresent();
+        assertThat(results.get().isbn()).isEqualTo(book.isbn());
+    }
+
+    @Test
+    void existsByIsbn() {
+    }
+
+    @Test
+    void deleteByIsbn() {
+    }
+}
